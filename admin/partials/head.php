79d2898bf0b6b03c4429c522bcffb616de9203e3
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2026-04-22 20:02:54 +0500

    some changes

--- admin/partials/head.php
+++ admin/partials/head.php
@@ -1,7 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
 
+$page_url = "http://localhost/nobility-frontend";
+
+
+?>
 <head>
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($page_title) ? $page_title : ''; ?></title>
